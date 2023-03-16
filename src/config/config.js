572d465aaf4d225b0b9a43ca613693febc8c7fd1
indacurdev <70963212+indacurdev@users.noreply.github.com>
2023-03-16 12:25:30 -0400

    update url services

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,5 @@
-export const API_URL    = 'http://127.0.0.1:8000/api';
-export const IMAGESURL  = 'http://127.0.0.1/Store/images';
-// export const API_URL = 'https://vagonmd.com/Store/wings-store/public/api';
+//export const API_URL    = 'http://127.0.0.1:8000/api';
+//export const IMAGESURL  = 'http://127.0.0.1/Store/images';
+
+export const IMAGESURL = 'https://vagonmd.com/Store/wings-store/';
+export const API_URL = 'https://vagonmd.com/Store/wings-store/public/api';
