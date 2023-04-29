1bc777d3207c3701daae163b8e05de07d7465d64
indacurdev <70963212+indacurdev@users.noreply.github.com>
2023-04-29 11:26:55 -0400

    first deploy

--- src/config/config.js
+++ src/config/config.js
@@ -3,3 +3,4 @@ export const IMAGESURL  = process.env.NEXT_PUBLIC_IMAGESURL;
 
 // export const IMAGESURL = 'https://vagonmd.com/Store/wings-store/images';
 // export const API_URL = 'https://vagonmd.com/Store/wings-store/public/api'; 
+// first deploy
