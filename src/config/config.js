e8e5df4ae2f350b9a83240e0d63e1c8079c301b4
indacurdev <70963212+indacurdev@users.noreply.github.com>
2023-04-29 11:23:24 -0400

    api url by enviroment variables

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-export const API_URL    = "https://vagonmd.com/Store/wings-store/public/api";
+export const API_URL    = process.env.NEXT_PUBLIC_API_URL;
 export const IMAGESURL  = process.env.NEXT_PUBLIC_IMAGESURL;
 
 // export const IMAGESURL = 'https://vagonmd.com/Store/wings-store/images';
